7dc54c1ce5f723a00f69809c2ad384e1a0ea8192
Levy <lnjiru43@gmail.com>
2023-11-13 15:54:32 +0300

    created contact page

---
+++ apps/eccomerce/app/contact/page.jsx
@@ -0,0 +1,11 @@
+import { Contact } from '@eccomerce/contact'
+
+const ContactPage = () => {
+    return (
+        <div>
+            <Contact />
+        </div>
+    )
+}
+
+export default ContactPage
