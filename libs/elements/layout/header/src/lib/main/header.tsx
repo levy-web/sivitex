98cf97425301f0909851b9c2fe184ba4e45b2254
Levy <lnjiru43@gmail.com>
2023-11-14 06:04:34 +0300

    fixed responsiveness

--- libs/elements/layout/header/src/lib/main/header.tsx
+++ libs/elements/layout/header/src/lib/main/header.tsx
@@ -7,28 +7,41 @@ export interface HeaderProps {}
 
 export const Header = () => {
     const [menu, setMenu] = useState<boolean>(false);
+    const [menuOpen, setMenuOpen] = useState(false);
     const pathname = usePathname()
 
     const toggleMenu = () => {
         setMenu(!menu)
     }
 
+    const toggleHamburgerMenu = () => {
+        setMenuOpen(!menuOpen);
+      };
+
   return (
 
     <nav className="bg-white fixed w-screen border-gray-200 dark:bg-gray-900 dark:border-gray-700">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <button data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" 
+                aria-controls="navbar-multi-level" 
+                aria-expanded={menuOpen}
+                onClick={toggleHamburgerMenu}
+            >
+                <span className="sr-only">Open main menu</span>
+                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
+                </svg>
+            </button>
             <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                 <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
             </Link>
-            <button data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false">
-                <span className="sr-only">Open main menu</span>
-                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                </svg>
-            </button>
-            <div className="hidden bg-red-200 mx-auto w-full md:block md:w-auto" id="navbar-multi-level">
-                <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <div className='flex md:hidden'>
+                <img className='h-6 w-6 cursor-pointer' src='/shopping-cart.png' alt=''/>
+                <span className='text-xs'>0</span>
+            </div>
+            <div className={`mx-auto w-full md:block md:w-auto ${menuOpen ? 'text-center' : 'hidden'}`} id="navbar-multi-level">
+                <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 rounded-lg bg-opacity-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li className='py-2 px-3'>
                         <Link href="/home" className={`py-2 px-3 rounded ${pathname === '/home' ? 'text-white bg-gray-900' : 'text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'}`}>Home</Link>
                     </li>
@@ -43,7 +56,7 @@ export const Header = () => {
                     </li>
                 </ul>
             </div>
-            <div className='flex'>
+            <div className='flex hidden md:flex'>
                 <img className='h-6 w-6 cursor-pointer' src='/shopping-cart.png' alt=''/>
                 <span className='text-xs'>0</span>
             </div>
