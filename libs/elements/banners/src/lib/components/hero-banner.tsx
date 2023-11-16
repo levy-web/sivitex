27dd122be9ce851b1b815aee478c2ab9fd7e7e9b
Levy <lnjiru43@gmail.com>
2023-11-16 23:13:05 +0300

    responsiveness hero display

--- libs/elements/banners/src/lib/components/hero-banner.tsx
+++ libs/elements/banners/src/lib/components/hero-banner.tsx
@@ -18,7 +18,30 @@ export const HeroBanner = (props: HeroBannerProps) => {
   }
   return (
     <div className='h-screen flex items-center justify-center'>
-        <div className='h-screen-20 flex items-center justify-center'>
+        <div className='md:hidden h-screen-20 flex items-center justify-center'>
+          <div className='h-full bg-gradient-to-r from-gray-800 to-transparent w-screen flex items-center justify-center'>
+
+              <div className='h-full flex items-center justify-center'>
+
+              <div className='absolute py-8 mx-auto'>
+                  <div>
+                    <div>
+                    <Texts {...textData}/>
+                    </div>
+                    <div>
+                      <NormalButton {...buttonData}/>
+                    </div>                
+                  </div>
+              </div>
+                
+                <img src='/model.png' alt='' className='w-3/5 h-3/5' />
+              </div>
+          </div>
+        </div>
+
+        {/* on mobile device */}
+
+        <div className='hidden md:block h-screen-20 flex items-center justify-center'>
           <div className='h-full bg-gradient-to-r from-gray-800 to-transparent w-screen flex items-center justify-center'>
 
               <div className='h-full flex items-center justify-center'>
