f11c83bd399072dbce58725794dcdf07af14bb3a
Levy <lnjiru43@gmail.com>
2023-11-17 18:52:04 +0300

    redirect to home route when in /

--- apps/eccomerce/app/page.tsx
+++ apps/eccomerce/app/page.tsx
@@ -1,12 +1,21 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
 export default async function Index() {
+
+  const router = useRouter();
+
+  React.useEffect(() => {
+    // Redirect to the /home route
+    router.push('/home');
+  }, []);
   /*
    * Replace the elements below with your own.
    *
    * Note: The corresponding styles are in the ./index.scss file.
    */
-  return (
-    <div className='h-screen container mx-auto p-20'>
-      <h2>hello world</h2>
-    </div>
-  );
+  
+  return <div>Redirecting...</div>;
+  
 }
