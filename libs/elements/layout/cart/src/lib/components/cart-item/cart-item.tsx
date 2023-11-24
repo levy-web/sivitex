24e2a58736fc19a0df2887c7a8c9ef86abd6a0a5
Levy <lnjiru43@gmail.com>
2023-11-24 14:54:22 +0300

    w-full

--- libs/elements/layout/cart/src/lib/components/cart-item/cart-item.tsx
+++ libs/elements/layout/cart/src/lib/components/cart-item/cart-item.tsx
@@ -19,21 +19,21 @@ export const CartItem: React.FC<CartItemProps> = (props) => {
 
   return (
     <div className='container flex flex-row items-center justify-center bg-gray-50 px-2 w-full mb-4 rounded-xl'>
-      <div className="w-16 h-16">
+      <div className="w-full h-full">
         <img src={imgSrc} alt={prodectName} className=""/>
       </div>
-      <div className="w-100 hidden md:block text-xl mx-auto">
+      <div className="w-full hidden md:block text-xl mx-auto">
         <p>{prodectName}</p>
       </div>
-      <div className="w-100 flex flex-row text-lg px-4 py-2 ms-auto justify-center items-center">
+      <div className="w-full flex flex-row text-lg px-4 py-2 ms-auto justify-center items-center">
         <p className="mx-2 cursor-pointer"> - </p>
         <p className="mx-2">{`${quantity}`}</p>
         <p className="mx-2 cursor-pointer"> + </p>
       </div>
-      <div className="w-100 text-lg px-4 ms-auto">
+      <div className="w-full text-lg px-4 ms-auto">
         <p>{`ksh ${price * quantity}`}</p>
       </div>
-      <div className="w-100 px-4">
+      <div className="w-full px-4">
         <button onClick={()=> dispatch(removeFromCart(id))} className="text-red-600">remove</button>
       </div>
     </div>
