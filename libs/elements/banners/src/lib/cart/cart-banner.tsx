2585e73554963a78cc1eabca15a09a5bb8cb4050
Levy <lnjiru43@gmail.com>
2023-11-28 07:38:06 +0300

    :lipstick:feat: cart design

--- libs/elements/banners/src/lib/cart/cart-banner.tsx
+++ libs/elements/banners/src/lib/cart/cart-banner.tsx
@@ -29,13 +29,16 @@ export function CartBanner(props: CartBannerProps) {
 
   return (
     <div className='min-h-screen justify-center items-center bg-gray-50'>
-      <div className="min-h-screen items-center justify-center flex flex-col pt-32">
-      {CartData.length > 0 && (<p className="mb-2 text-gray-600 text-lg">Thanks for  shopping at sivitex.</p>)}
-      {CartData.length > 0 && (<p className="mb-2 text-gray-600 text-lg text-center">We will contact you when your order is on the way.</p>)}
-      <div className="container pt-12 mx-auto flex flex-col md:flex-row w-full">
-      { CartData.length > 0 ? (<div className="w-full md:w-2/3 min-h-80 md:mx-8">
+      <div className="min-h-screen items-center justify-center flex flex-col pt-14">
+      {/* {CartData.length > 0 && (<p className="mb-2 text-gray-600 text-lg">Thanks for  shopping at sivitex.</p>)}
+      {CartData.length > 0 && (<p className="mb-2 text-gray-600 text-lg text-center">We will contact you when your order is on the way.</p>)} */}
+      <div className="container pt-4 mx-auto flex flex-col md:flex-row w-full">
+      { CartData.length > 0 ? 
+        (<div className="w-full md:w-2/3 min-h-80 md:mx-8 px-2">
+          <h2 className="py-2 px-2 w-full bg-light">Cart Items</h2>
            {CartItems} 
-        </div>) : <EmptyCart/> }
+        </div>) 
+        : <EmptyCart/> }
         {CartData.length > 0 && (<div className="w-full md:w-1/3 h-full mt-4 md:mt-0 mb-4 bg-gray-200  rounded-xl items-center flex flex-col">
           <Checkout/>
         </div>)}
--- libs/elements/layout/cart/src/lib/components/cart-item/cart-item.tsx
+++ libs/elements/layout/cart/src/lib/components/cart-item/cart-item.tsx
@@ -18,22 +18,23 @@ export const CartItem: React.FC<CartItemProps> = (props) => {
 
 
   return (
-    <div className='container flex flex-row items-center justify-center bg-gray-50 px-2 w-full mb-4 rounded-xl'>
+    <div className='container border-b-2 flex flex-col md:flex-row items-center justify-center bg-white px-1 w-full mb-4 rounded-xl'>
+      <p onClick={()=> dispatch(removeFromCart(id))} className="ms-auto md:hidden text-3xl text-red-600 mr-4">x</p>
       <div className="w-full h-full">
         <img src={imgSrc} alt={prodectName} className=""/>
       </div>
-      <div className="w-full hidden md:block text-xl mx-auto">
+      <div className="w-full text-xl text-center mx-auto">
         <p>{prodectName}</p>
       </div>
-      <div className="w-full flex flex-row text-lg px-4 py-2 ms-auto justify-center items-center">
-        <p className="mx-2 cursor-pointer"> - </p>
+      <div className="w-full flex flex-row text-lg px-2 py-2 ms-auto justify-center items-center">
+        <p className="mx-2 cursor-pointer bg-gray-200 px-3 py-1"> - </p>
         <p className="mx-2">{`${quantity}`}</p>
-        <p className="mx-2 cursor-pointer"> + </p>
+        <p className="mx-2 cursor-pointer bg-gray-200 px-3 py-1"> + </p>
       </div>
-      <div className="w-full text-lg px-4 ms-auto">
+      <div className="w-full text-lg text-center px-2 ms-auto">
         <p>{`ksh ${price * quantity}`}</p>
       </div>
-      <div className="w-full px-4">
+      <div className="w-full hidden md:block text-center pl-2">
         <button onClick={()=> dispatch(removeFromCart(id))} className="text-red-600">remove</button>
       </div>
     </div>
